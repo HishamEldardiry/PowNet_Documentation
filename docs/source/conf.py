fa5d2771f51d3aef66c439824cbbe40c9ba5edea
Hisham Eldardiry <35979167+HishamEldardiry@users.noreply.github.com>
2024-05-01 11:11:04 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,12 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+html_static_path = ['_static']
 
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
